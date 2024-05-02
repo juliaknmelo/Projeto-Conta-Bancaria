4cedc3126ff94d0736dae0c9094339ebfe2342d8
juliaknmelo <ju-kar98@hotmail.com>
2024-05-02 15:28:13 -0300

    atualização 02.05

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,26 +1,37 @@
 package conta;
 
+
 import java.util.Scanner;
-import conta.model.Conta;
 import conta.util.Cores;
+import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
 
 public class Menu {
 
+	public static Scanner leia = new Scanner (System.in);
+	
 	public static void main(String[] args) {
 		
-		//teste da classe conta
-		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
-		c1.visualizar();
-		c1.sacar(12000.0f);
-		c1.visualizar();
-		c1.depositar(5000.0f);
-		c1.visualizar();
-		
-		
-		Scanner leia = new Scanner(System.in);
-
 		int opcao;
-
+				
+		//TESTE CONTA CORRENTE
+		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "Mariana", 15000.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);;
+		cc1.visualizar();
+		
+		//TESTE CONTA POUPANÇA
+		ContaPoupanca cp1 = new ContaPoupanca (3, 123, 2, "Victor", 100000.0f, 15);
+		cp1.visualizar();
+		cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
+		
 		while (true) {
 			
 			System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
---
+++ src/conta/repository/ContaRepository.java
@@ -0,0 +1,19 @@
+package conta.repository;
+
+import conta.model.Conta;
+
+public interface ContaRepository {
+	
+	//CRUD DA CONTA
+	public void produrarPorNumero(int numero);
+	public void listarTodas();
+	public void cadastrar (Conta conta);
+	public void atualizar (Conta conta);
+	public void deletar (int numero);
+	
+	//METODOS BANCARIOS
+	public void sacar (int numero, float valor);
+	public void depositar (int numero, float valor);
+	public void transferir (int numeroOrigem, int numeroDestino, float valor);
+
+}
